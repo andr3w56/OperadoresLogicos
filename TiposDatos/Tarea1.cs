53ac2876710f07e9369a9bdcf5ec3adc9f0cbf56
andr3w56 <andrewgonzale009@gmail.com>
2022-11-01 14:30:06 -0500

    Tarea #1

---
+++ TiposDatos/Tarea1.cs
@@ -0,0 +1,16 @@
+using System;
+
+					
+public class Program
+{
+	public static void Main()
+	{
+		string NombreCompleto = "Andrew Fernando González Castillo";
+		string FechaDeNacimiento = "28/07";
+		byte Edad = 18;
+		double Estatura = 1.67;
+		char Genero = 'M';
+		string Carrera = "TIC";
+		Console.Write("Nombre Completo: {0}\nFecha De Nacimiento: {1}\nEdad = {2}\nEstatura = {3}\nGénero: {4}\nCarrera: {5}", NombreCompleto, FechaDeNacimiento, Edad, Estatura, Genero, Carrera);
+	}
+}

473ed742bbac245265c1f1becdd2b68dd010a0c6
andr3w56 <andrewgonzale009@gmail.com>
2022-11-01 14:42:55 -0500

    Corrigiendo Peso

--- TiposDatos/Tarea1.cs
+++ TiposDatos/Tarea1.cs
@@ -1,16 +1,17 @@
 using System;
 
-					
+                                        
 public class Program
 {
-	public static void Main()
-	{
-		string NombreCompleto = "Andrew Fernando González Castillo";
-		string FechaDeNacimiento = "28/07";
-		byte Edad = 18;
-		double Estatura = 1.67;
-		char Genero = 'M';
-		string Carrera = "TIC";
-		Console.Write("Nombre Completo: {0}\nFecha De Nacimiento: {1}\nEdad = {2}\nEstatura = {3}\nGénero: {4}\nCarrera: {5}", NombreCompleto, FechaDeNacimiento, Edad, Estatura, Genero, Carrera);
-	}
-}
+        public static void Main()
+        {
+                string NombreCompleto = "Andrew Fernando González Castillo";
+                string FechaDeNacimiento = "28/07";
+                byte Edad = 18;
+                string Peso = "25kg";
+                double Estatura = 1.67;
+                char Genero = 'M';
+                string Carrera = "TIC";
+                Console.Write("Nombre Completo: {0}\nFecha De Nacimiento: {1}\nEdad = {2}\nPeso: {3}\nEstatura = {4}\nGénero: {5}\nCarrera: {6}", NombreCompleto, FechaDeNacimiento, Edad, Peso, Estatura, Genero, Carrera);
+        }
+} 

5c9b07f9efdd118c44dac0ee50463933025b14cc
andr3w56 <andrewgonzale009@gmail.com>
2022-11-02 19:23:45 -0500

    added boolean data

--- TiposDatos/Tarea1.cs
+++ TiposDatos/Tarea1.cs
@@ -8,10 +8,18 @@ public class Program
                 string NombreCompleto = "Andrew Fernando González Castillo";
                 string FechaDeNacimiento = "28/07";
                 byte Edad = 18;
-                string Peso = "25kg";
+                string Peso = "65kg";
                 double Estatura = 1.67;
                 char Genero = 'M';
-                string Carrera = "TIC";
-                Console.Write("Nombre Completo: {0}\nFecha De Nacimiento: {1}\nEdad = {2}\nPeso: {3}\nEstatura = {4}\nGénero: {5}\nCarrera: {6}", NombreCompleto, FechaDeNacimiento, Edad, Peso, Estatura, Genero, Carrera);
+		bool ifCarrera = true;
+		Console.Write("Nombre Completo: {0}\nFecha De Nacimiento: {1}\nEdad = {2}\nPeso: {3}\nEstatura = {4}\nGénero: {5}\n", NombreCompleto, FechaDeNacimiento, Edad, Peso, Estatura, Genero);
+
+		if (ifCarrera)
+		{
+			Console.Write("¿Es de TICS?: Si");
+		}
+		Console.ReadKey();
+		
+
         }
 } 

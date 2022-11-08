b9f5dffba82d0bf65edf52f52c566c89a1e01ec6
andr3w56 <58153210+andr3w56@users.noreply.github.com>
2022-11-08 12:30:10 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementoDecremento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese nota final: ");
+            byte NotaFinal = Convert.ToByte(Console.ReadLine());
+            Console.Write("Ingrese nota: ");
+            byte Nota = Convert.ToByte(Console.ReadLine());
+            string res = (Nota >= NotaFinal) ? "Haz alcazado la nota final." : "No alcanzaste la nota final.";
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}

e7de3c83f65b4056ec6995c25b1894ff394a3e0a
andr3w56 <andrewgonzale009@gmail.com>
2022-11-08 16:06:01 -0500

    gitignote

--- Program.cs
+++ Program.cs
@@ -11,13 +11,21 @@ namespace IncrementoDecremento
     {
         static void Main(string[] args)
         {
-            Console.Write("Ingrese nota final: ");
+            /*Console.Write("Ingrese nota final: ");
             byte NotaFinal = Convert.ToByte(Console.ReadLine());
             Console.Write("Ingrese nota: ");
             byte Nota = Convert.ToByte(Console.ReadLine());
             string res = (Nota >= NotaFinal) ? "Haz alcazado la nota final." : "No alcanzaste la nota final.";
             Console.WriteLine(res);
-            Console.ReadKey();
+            Console.ReadKey();*/
+	    
+	    for (int i; i <= 12; i++)
+	    {
+		    ConsoleWriteLine$(R"""if (NumeroAleatorio == i)
+				    {
+	ConsoleWrite("meses[{i}]"
+}""");
+	    }
         }
     }
 }

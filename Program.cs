2205605bbd7dd6d3073e1246df614da531cd504e
andr3w56 <andrewgonzale009@gmail.com>
2022-11-08 16:53:35 -0500

    to mainA

--- Program.cs
+++ Program.cs
@@ -18,14 +18,62 @@ namespace IncrementoDecremento
             string res = (Nota >= NotaFinal) ? "Haz alcazado la nota final." : "No alcanzaste la nota final.";
             Console.WriteLine(res);
             Console.ReadKey();*/
-	    
-	    for (int i; i <= 12; i++)
-	    {
-		    ConsoleWriteLine$(R"""if (NumeroAleatorio == i)
-				    {
-	ConsoleWrite("meses[{i}]"
-}""");
-	    }
+
+            Random rnd = new Random();
+            int Aleatorio = rnd.Next(1, 12);
+
+            Console.WriteLine($"El numero random es: {Aleatorio}");
+
+            string[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+            "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+            if (Aleatorio == 1)
+            {
+                Console.WriteLine(meses[0]);
+            }
+            if (Aleatorio == 2)
+            {
+                Console.WriteLine(meses[1]);
+            }
+            if (Aleatorio == 3)
+            {
+                Console.WriteLine(meses[2]);
+            }
+            if (Aleatorio == 4)
+            {
+                Console.WriteLine(meses[3]);
+            }
+            if (Aleatorio == 5)
+            {
+                Console.WriteLine(meses[4]);
+            }
+            if (Aleatorio == 6)
+            {
+                Console.WriteLine(meses[5]);
+            }
+            if (Aleatorio == 7)
+            {
+                Console.WriteLine(meses[6]);
+            }
+            if (Aleatorio == 8)
+            {
+                Console.WriteLine(meses[7]);
+            }
+            if (Aleatorio == 9)
+            {
+                Console.WriteLine(meses[8]);
+            }
+            if (Aleatorio == 10)
+            {
+                Console.WriteLine(meses[9]);
+            }
+            if (Aleatorio == 11)
+            {
+                Console.WriteLine(meses[10]);
+            }
+            if (Aleatorio == 12)
+            {
+                Console.WriteLine(meses[11]);
+            }
         }
     }
 }

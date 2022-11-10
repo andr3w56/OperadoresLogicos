bc07f28c4a18821486914c0ed3d5fc6ca06cb795
andr3w56 <andrewgonzale009@gmail.com>
2022-11-10 11:52:34 -0500

    Ejemplo operador lógico Y

---
+++ helloWorld/OperadoresLogicos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicsOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicios con operadores lógicos");
+            string genero;
+            int edad;
+            Console.WriteLine("Ingresa tu género (h-m)");
+            genero = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu edad");
+            edad = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa tu nombre");
+            string nombre = Console.ReadLine();
+
+            if (edad >= 18 && genero == "h")
+            {
+                Console.WriteLine($"Hola señor {nombre}, eres mayor de edad y tienes {edad} años.");
+            }
+            else if (edad < 18 && genero == "h")
+            {
+                Console.WriteLine($"Hola joven {nombre} eres menor de edad y tiene {edad} años.");
+            }
+
+        }
+    }
+}
